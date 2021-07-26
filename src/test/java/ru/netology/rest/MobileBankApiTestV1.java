61fb23c8cf0ec382fd9e5b7b667fde227235baa3
Nadya Zykova <xilda777@gmail.com>
2021-07-26 10:26:55 +0400

    test ok

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -17,7 +17,7 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(201)
+                .statusCode(200)
                 // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath.
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
         ;
